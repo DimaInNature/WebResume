7484c3713bf31b913cbe5a93cab764aad5f255cf
Dmitry Nikulin <Nikulin.d@dobrozaim.ru>
2022-07-19 08:14:03 +0300

    Added a universal static class for adding shared generic repositories to the container.

---
+++ WR.Shared.Persistence/IoC/PersistenceDependencyInjectionConfiguration.cs
@@ -0,0 +1,28 @@
+﻿namespace WR.Shared.Persistence.IoC
+{
+    public static class PersistenceDependencyInjectionConfiguration
+    {
+        public static void AddSharedPersistence(this IServiceCollection services)
+        {
+            _ = services ?? throw new ArgumentNullException(paramName: nameof(services));
+
+            services.AddTransient(serviceType: typeof(IGenericRepository<>), implementationType: typeof(GenericRepository<>));
+
+            services.Decorate(serviceType: typeof(IGenericRepository<>), decoratorType: typeof(GenericRepositoryLoggerDecorator<>));
+        }
+
+        public static void AddRepositoriesFromSharedPersistence(this IServiceCollection services)
+        {
+            _ = services ?? throw new ArgumentNullException(paramName: nameof(services));
+
+            services.AddTransient(serviceType: typeof(IGenericRepository<>), implementationType: typeof(GenericRepository<>));
+        }
+
+        public static void AddLoggingFromSharedPersistence(this IServiceCollection services)
+        {
+            _ = services ?? throw new ArgumentNullException(paramName: nameof(services));
+
+            services.AddTransient(serviceType: typeof(IGenericRepository<>), implementationType: typeof(GenericRepositoryLoggerDecorator<>));
+        }
+    }
+}
--- WR.Shared.Persistence/global.cs
+++ WR.Shared.Persistence/global.cs
@@ -2,3 +2,6 @@
 global using Microsoft.Extensions.Logging;
 global using System.Linq.Expressions;
 global using WR.Shared.Persistence.Interfaces;
+global using Microsoft.Extensions.DependencyInjection;
+global using WR.Shared.Persistence.Decorators;
+global using WR.Shared.Persistence.Repositories;
