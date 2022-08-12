2a3be9d2a7dec867d2b851361ab9cd044f534168
dimainnature <dimainnature@yandex.ru>
2022-08-12 06:54:37 +0400

    Feat: Add design to ReadUsersView.

--- WR.Consumers.Desktop.Presentation/global.cs
+++ WR.Consumers.Desktop.Presentation/global.cs
@@ -28,6 +28,7 @@ global using WR.Consumers.Desktop.Presentation.ViewModels.UserControls.PetProjec
 global using WR.Consumers.Desktop.Presentation.ViewModels.UserControls.UserRoles;
 global using WR.Consumers.Desktop.Presentation.Views;
 global using WR.Consumers.Desktop.Presentation.Views.UserControls.ContactMessages;
+global using WR.Consumers.Desktop.Presentation.Views.UserControls.Menus;
 global using WR.Consumers.Desktop.Presentation.Views.UserControls.UserRoles;
 global using WR.Consumers.Desktop.Presentation.Views.UserControls.Users;
 global using ThisApplication = System.Windows.Application;
